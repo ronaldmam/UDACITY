f8d7168586767c0a6428c71953b4cf178c1cd00f
ronald <ronaldmam@yahoo.es>
2016-08-21 19:31:34 -0500

    Es es el segundo commit que hago

--- src/oop1/pro2/uml/app.java
+++ src/oop1/pro2/uml/app.java
@@ -7,14 +7,9 @@ import oop1.pro2.uml.controller.Customer.CustomerCreateController;
 
 public class app {
 	public static void main(String[] agrs){
-		//Controller customerCreateController=new CustomerCreateController();
-		//customerCreateController.processRequest();
-		String[] valores={"Ronald", "Carlos", "Victor", "Roger"};
-		for (String val:valores)
-		{
-			System.out.println(val);
-		}
-		
+		Controller customerCreateController=new CustomerCreateController();
+		customerCreateController.processRequest();
+
 	}
 
 }
